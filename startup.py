a73ed2ceafaba652ce9ecce3c4c2f4ae5e6ba295
pocketcat3 <114851989+pocketcat3@users.noreply.github.com>
2022-12-18 15:49:54 +0200

    Delete startup.py

--- startup.py
+++
@@ -1,7 +0,0 @@
-from winreg import *
-key_my = OpenKey(HKEY_CURRENT_USER, 
-                 r'SOFTWARE\Microsoft\Windows\CurrentVersion\Run', 
-                 0, KEY_ALL_ACCESS)
-
-SetValueEx(key_my, 'script', 0, REG_SZ, r'main.py')
-CloseKey(key_my)
